d8a8aaf0648d2c8f10624939ade83e1a64d5c08c
Tarachand Gupta <tarachandragupta2784@gmail.com>
2021-09-27 17:08:51 +0530

    working so changing it back to normal

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Student. EES tara
+         Student. EES
         </p>
         <a
           className="App-link"
