d212edcc27850ceaa1ff1ef55ef6402a53ff5b90
Tarachand Gupta <tarachandragupta2784@gmail.com>
2021-10-01 16:17:50 +0530

    service worker added

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import "./tailwind.css";
 import App from './App';
 import { Provider } from "react-redux";
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import store from './app/Store/MainStore';
 
@@ -19,4 +20,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorker.register();
 reportWebVitals();
