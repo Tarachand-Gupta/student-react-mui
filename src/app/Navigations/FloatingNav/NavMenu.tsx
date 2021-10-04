62cb9e7f63d13223bcf3bcb0f8ff0d2c9b7f42bb
Tarachand Gupta <tarachandragupta2784@gmail.com>
2021-10-04 19:15:03 +0530

    es lint !

--- src/app/Navigations/FloatingNav/NavMenu.tsx
+++ src/app/Navigations/FloatingNav/NavMenu.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import FloatingNav from './FloatingNav';
 import { FcHome, FcSms, FcViewDetails, FcTemplate, FcBullish, FcDataSheet, FcBusinessman, FcSettings } from "react-icons/fc";
 
 
